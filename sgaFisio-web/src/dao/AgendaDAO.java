457b0544d8995181ebba1fd4c04f8db048890c80
MarcosMoraes23 <marcosmoraes2303@gmail.com>
2015-06-21 11:42:22 -0300

    AgendaDAO e Persistence Util Adicionados

--- sgaFisio-web/src/dao/AgendaDAO.java
+++ sgaFisio-web/src/dao/AgendaDAO.java
@@ -1,6 +1,108 @@
 package dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import model.Agenda;
+import util.PersistenceUtil;
+
 public class AgendaDAO 
 {
+	public static AgendaDAO getInstance()
+	{
+		return new AgendaDAO();
+	}
+		
+//PERSISTIR	
+	public Agenda persistir(Agenda agenda)
+	{
+		EntityManager em =  PersistenceUtil.getEntityManager();
+		
+		try 
+		{
+			em.getTransaction().begin();
+			agenda = em.merge(agenda);
+			em.getTransaction().commit();
+			
+			return agenda;
+			
+		} 
+		catch (Exception e) 
+		{
+			em.getTransaction().rollback();
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+//BUSCAR TODOS
+	@SuppressWarnings("unchecked")
+	public List<Agenda> buscarTodos()
+	{
+		List<Agenda> autores;
+		
+		EntityManager em =  PersistenceUtil.getEntityManager();
+		
+		try 
+		{
+			Query query = em.createNamedQuery("Agenda.FindAll");
+			autores = query.getResultList();
+			
+			return autores;
+			
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+//BUSCAR POR ID	
+	public Agenda buscarId(Long id)
+	{
+		EntityManager em = PersistenceUtil.getEntityManager();
+		Agenda agenda;
+		
+		try
+		{
+			Query query = em.createQuery("SELCT a from Agenda a where id=:id");
+			query.setParameter("id", id);
+			
+			agenda= (Agenda)query.getSingleResult();
+			
+			return agenda;
+						
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	
+//EXCLUIR
+	public boolean excluir(Agenda agenda)
+	{
+		EntityManager em = PersistenceUtil.getEntityManager();
+		
+		try
+		{
+			em.getTransaction().begin();
+			em.remove(agenda);
+			em.getTransaction().commit();
+			return true;
+		}
+		catch(Exception e)
+		{
+			em.getTransaction().rollback();
+			e.printStackTrace();
+			return false;
+		}
+	}
 
 }
