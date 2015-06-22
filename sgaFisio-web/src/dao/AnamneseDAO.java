d06e4952e6b7b750072d49e8843512e619ca9dd5
MarcosMoraes23 <marcosmoraes2303@gmail.com>
2015-06-22 15:24:15 -0300

    Bootstrap

--- sgaFisio-web/src/dao/AnamneseDAO.java
+++ sgaFisio-web/src/dao/AnamneseDAO.java
@@ -1,5 +1,41 @@
 package dao;
 
-public class AnamneseDAO {
+import javax.persistence.EntityManager;
 
+import util.PersistenceUtil;
+import model.Anamnese;
+
+public class AnamneseDAO 
+{
+	public AnamneseDAO getInstance()
+	{
+		return new AnamneseDAO();
+	}
+
+	
+//PERSISTIR 
+	public Anamnese persistir(Anamnese anamnese)
+	{
+		EntityManager em = PersistenceUtil.getEntityManager();
+		
+		
+		try
+		{
+			em.getTransaction().begin();
+			anamnese = em.merge(anamnese);
+			em.getTransaction().commit();
+			
+			return anamnese;
+		}
+		catch (Exception e)
+		{
+			em.getTransaction().rollback();
+			e.printStackTrace();
+			return null;
+		}
+	}
+//BUSCAR POR NOME
+//BUSCAR POR ID
+//BUSCAR POR DATA
+//EXCLUIR	
 }
